05f0978035f1622ae0b93d8a12311cb220c8f807
Aaron So <zaiyou12@gmail.com>
2023-05-22 21:38:24 +0900

    ✅ Add Article tests

--- web/src/components/Article/Article.tsx
+++ web/src/components/Article/Article.tsx
@@ -2,19 +2,26 @@ import type { Post } from 'types/graphql'
 
 import { Link, routes } from '@redwoodjs/router'
 
-interface Props {
-  article: Post
+const truncate = (text: string, length: number) => {
+  return text.substring(0, length) + '...'
 }
 
-const Article = ({ article }: Props) => {
+interface Props {
+  article: Omit<Post, 'createdAt'>
+  summary?: boolean
+}
+
+const Article = ({ article, summary = false }: Props) => {
   return (
     <article>
       <header>
-        <h2 className="text-xl text-purple-700 font-semibold">
+        <h2 className="text-xl font-semibold text-purple-700">
           <Link to={routes.article({ id: article.id })}>{article.title}</Link>
         </h2>
       </header>
-      <div className="mt-2 text-gray-900 font-light">{article.body}</div>
+      <div className="mt-2 font-light text-gray-900">
+        {summary ? truncate(article.body, 100) : article.body}
+      </div>
     </article>
   )
 }
