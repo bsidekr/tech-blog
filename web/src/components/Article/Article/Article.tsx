066035ace1a0e93a4b84525bd73b28ce1fc74544
Aaron So <zaiyou12@gmail.com>
2023-06-14 23:36:40 +0900

    💄 Update articlePage with tailwind Typography

--- web/src/components/Article/Article/Article.tsx
+++ web/src/components/Article/Article/Article.tsx
@@ -1,9 +1,9 @@
+import { clsx } from '@mantine/core'
 import type { FindArticleListQuery } from 'types/graphql'
-
-import { Link, routes } from '@redwoodjs/router'
 
 import CommentForm from 'src/components/CommentForm'
 import CommentsCell from 'src/components/CommentsCell'
+import { intlDateTimeFormat } from 'src/lib/formatters'
 
 interface Props {
   article: FindArticleListQuery['articles'][0]
@@ -12,15 +12,17 @@ interface Props {
 const Article = ({ article }: Props) => {
   return (
     <article>
-      <header>
-        <h2 className="text-xl font-semibold text-purple-700">
-          <Link to={routes.article({ id: article.id })}>{article.title}</Link>
-          <span className="ml-2 font-normal text-gray-400">
-            by {article.user.name}
-          </span>
-        </h2>
+      <header className="flex flex-col">
+        <h1 className="mt-6">{article.title}</h1>
+        <time
+          dateTime={article.createdAt}
+          className="order-first flex items-center text-base text-zinc-400 dark:text-zinc-500"
+        >
+          <span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500" />
+          <span className="ml-3">{intlDateTimeFormat(article.createdAt)}</span>
+        </time>
       </header>
-      <div className="mt-2 font-light text-gray-900">{article.body}</div>
+      <Prose className="mt-8">{article.body}</Prose>
       <div className="mt-12">
         <CommentForm postId={article.id} />
         <div className="mt-12">
@@ -31,4 +33,12 @@ const Article = ({ article }: Props) => {
   )
 }
 
+const Prose = ({ children, className }) => {
+  return (
+    <div
+      className={clsx(className, 'prose dark:prose-invert')}
+      dangerouslySetInnerHTML={{ __html: children }}
+    ></div>
+  )
+}
 export default Article
